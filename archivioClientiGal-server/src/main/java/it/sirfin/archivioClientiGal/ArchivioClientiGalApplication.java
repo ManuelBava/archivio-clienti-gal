f0df33365355b2defb44212c922858345ec4ea42
FedericoRez <78588276+FedericoRez@users.noreply.github.com>
2021-03-06 10:50:44 +0100

    Creata cartella Spring + dipendenze
    #1

---
+++ archivioClientiGal-server/src/main/java/it/sirfin/archivioClientiGal/ArchivioClientiGalApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.archivioClientiGal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivioClientiGalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivioClientiGalApplication.class, args);
+	}
+
+}
